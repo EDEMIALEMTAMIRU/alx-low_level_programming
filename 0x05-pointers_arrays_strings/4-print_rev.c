fb48a006a199df2b156358710e55fa2c1ba12530
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-19 10:59:45 -0700

    a function that prints a string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev -  prints a string, in reverse, 
+ * @s: input parameter
+ * Return: 0
+ */
+void print_rev(char *s)
+{
+	int i;
+
+	i = 0;
+	while (s[i] != '\n')
+	{
+		i++;
+	}
+	--i;
+	for (i ; i >= 0; i--)
+	{
+		printf("%c", s[i]);
+	}
+	printf("\n");
+}
