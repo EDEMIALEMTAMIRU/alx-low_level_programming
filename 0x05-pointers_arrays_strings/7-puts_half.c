5295165a55b80b6082a8adff491757afe7769821
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-19 12:06:06 -0700

    the second hhalf

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts_half - prints half of a string
+ * @str: input
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+	int i, j;
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++
+	}
+	j = i / 2;
+
+	while (j < i)
+	{
+		_putchar(s[j]);
+		j++;
+	}
+}
