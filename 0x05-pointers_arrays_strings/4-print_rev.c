c2c0732040f0dc8073f472265db48b2543e56381
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-19 11:17:37 -0700

    a function that prints a string

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -10,13 +10,15 @@ void print_rev(char *s)
 	int i;
 
 	i = 0;
-	while (s[i] != '\n')
+	while (s[i] != '\0')
 	{
 		i++;
 	}
-	for (i--; i >= 0; i--)
+	--i;
+	while (i >= 0)
 	{
-		_putchar(s[i]);
+		printf("%c", s[i]);
+		i--;
 	}
 	printf("\n");
 }
