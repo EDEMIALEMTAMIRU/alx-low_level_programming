dc59932f2c68884de53fdbbd2397b952b0799ecb
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-21 12:52:57 -0700

    string concactination

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,11 +16,12 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		i++;
 	}
-	while (j <= n && src[j] != '\0')
+	while (n > 0 && src[j] != '\0')
 	{
 		dest[i] = src[j];
 		j++;
 		i++;
+		n--;
 	}
 	return (dest);
 }
