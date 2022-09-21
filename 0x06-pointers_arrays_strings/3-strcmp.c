30b1654da5823f6bde0892893505dc53d22e84fe
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-21 14:15:29 -0700

    string concactination

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,14 +1,14 @@
-#include <stdio.h>
+#include <stdio.h
 #include "main.h"
 /**
  * _strcmp - function that compares string comparation
- * @s1: input 
+ * @s1: input
  * @s2: input string
  * Return: Always 0
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j;
+	int i, j, x;
 
 	i = j = 0;
 	while (s1[i] != '\0')
@@ -20,10 +20,10 @@ int _strcmp(char *s1, char *s2)
 		j++;
 	}
 	if (i > j)
-		return (15);
+		x = 15;
 	else if (i == j)
-		return (0);
-
+		x = 0;
 	else
-		return (-15);
+		x = -15;
+	return (x);
 }
