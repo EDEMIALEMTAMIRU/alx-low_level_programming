5e07f56da3ead539c1e8e1936ccf8e940c0556b8
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-21 03:01:14 -0700

    string concactination

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _strncat - string concactination
+ * @dest: string one to be appended
+ * @src: string two which 
+ * @n: number of string appended
+ * Return: always 0
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int i, j;
+
+	i = j = 0;
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (j <= n)
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
