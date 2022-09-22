0d30f39595aebac170787af107937c3020a85b7c
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-22 01:11:51 -0700

    lowercase to uppercase

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -12,10 +12,8 @@ char string_toupper(char str)
 	i = 0;
 	while (str[i] != '\0')
 	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] = str[i] + 32;
-		else
-			str[i] = str[i];
+		if (str[i] >= 97 && str[i] <= 122)
+			str[i] = str[i] - 32;
 		i++;
 	}
 	return (str);
