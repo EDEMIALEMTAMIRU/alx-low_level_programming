1051234f39044a5619a3b0c1c252131f6019ee30
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-22 00:18:46 -0700

    string comparition

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,22 +8,14 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int i, j, x;
+	int i;
 
-	i = j = 0;
-	while (s1[i] != '\0')
+	i = 0;
+	while (s1[i] != '\0' || s2[i] != '\0')
 	{
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 		i++;
 	}
-	while (s2[j] != '\0')
-	{
-		j++;
-	}
-	if (i > j)
-		x = 15;
-	else if (i == j)
-		x = 0;
-	else
-		x = -15;
-	return (x);
+	return (0);
 }
