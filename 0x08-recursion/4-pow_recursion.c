b0547962fba18c6a029b40f4cf342f8c347a1424
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-28 15:09:03 -0700

    function that raised

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * _pow_recursion - power of a numbers
+ * @x: base input number
+ * @y: raised input number
+ * Return: always 0
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	else if (y == 0)
+		return (1);
+	else 
+		return (x * _pow_recursion(x, y - 1));
+}
