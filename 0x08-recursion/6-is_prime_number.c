3f466f09497d3e56d3c38d62a70ac5650d5a8408
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-28 16:46:10 -0700

    prime number

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * check_prime - check primeness
+ * @n: input numper
+ * @i: multiplier
+ * Return: always 0
+ */
+int check_prime(int n, int i)
+{
+	if (i == n - 1)
+		return (1);
+	else if (n % i == 0)
+		return (0);
+	if (n % i != 0)
+	{
+		return(check_prime(n, i + 1));
+	}
+	return (0);
+}
+/**
+ * is_prime_number - is prime
+ * @n: input number
+ */
+int is_prime_number(int n)
+{
+	int i = 2;
+
+	if (n < 2)
+		return (0);
+	if (n == 2)
+		return (1);
+	return(check_prime(n, i));
+}
