816a904d4fe2213eae202a11944a74c782e6c90c
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-28 15:58:36 -0700

    square root function

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+/**
+ * _sqrt - square root of a number
+ * @n: input number
+ * @i: root
+ * Return: always 0
+ */
+
+int _sqrt(int n, int i)
+{
+	if (n == 0 || n == 1)
+		return (n);
+	else if (i * i < n)
+		return (_sqrt(n, i + 1));
+	else if  (i * i == n)
+		return (i);
+	return (-1);
+}
+/**
+ * _sqrt_recursion - square root of a number
+ * @n: input number
+ * Return: always 0                                               
+ */     
+int _sqrt_recursion(int n)
+{
+	int i;
+
+	i = 0;
+	if (n < 0)
+		return (-1);
+	else
+		return (_sqrt(n, i));
+}
