b908395e250d6e6a61be966d451936dc23dba067
EDEMIALEMTAMIRU <edemialem.tamiru@ju.edu.et>
2022-09-30 06:20:51 -0700

    argument count

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * main - main function
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc);
+	return (0);
+}
