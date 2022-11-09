29a3f2ee889402087bd5f029608ebd86b8baf6f1
EDEMIALEMTAMIRU <111135990+EDEMIALEMTAMIRU@users.noreply.github.com>
2022-11-09 22:12:23 +0300

    Update 3-op_functions.c

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,31 +1,72 @@
 #include "3-calc.h"
+#include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-
 /**
- * get_op_func - ...
- * @s: ...
+ *op_add - adds two int
  *
- * Return: ...
+ *@a:int param
+ *@b:int param
+ *Return:int
  */
-int (*get_op_func(char *s))(int, int)
+int op_add(int a, int b)
 {
-	op_t ops[] = {
-	{ "+", op_add },
-	{ "-", op_sub },
-	{ "*", op_mul },
-	{ "/", op_div }
-	{ "%", op_mod },
-	{ NULL, NULL }
-	};
-	int i = 0;
-
-	while (i < 5)
+	return (a + b);
+}
+/**
+ *op_sub - returns diff of a and b
+ *
+ *@a:int param
+ *@b:int param
+ *
+ *Return:difference
+ */
+int op_sub(int a, int b)
+{
+	return (a - b);
+}
+/**
+ *op_mul - returns product of a and b
+ *
+ *@a:int
+ *@b:int
+ *
+ *Return:product
+ */
+int op_mul(int a, int b)
+{
+	return (a * b);
+}
+/**
+ *op_div - returns result of div of a and b
+ *
+ *@a:int
+ *@b:int
+ *
+ *Return:div
+ */
+int op_div(int a, int b)
+{
+	if (!b)
 	{
-		if (strcmp(s, ops[i].op) == 0)
-			return (ops[i].f);	
-		i++;
+		printf("Error\n");
+		exit(100);
 	}
-
-	return (0);
+	return (a / b);
+}
+/**
+ *op_mod - returns modulus of a and b
+ *
+ *@a:int
+ *@b:int
+ *
+ *Return: modulus
+ */
+int op_mod(int a, int b)
+{
+	if (!b)
+	{
+		printf("Error\n");
+		exit(100);
+	}
+	return (a % b);
 }
